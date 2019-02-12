2c1cecbcdc47ab8842aecb0674accec551131c4a
Aparicio, Ernie <ernesto.aparicio@optum.com>
2019-02-12 15:14:10 -0800

    Implement client

--- blog/blog_client/client.go
+++ blog/blog_client/client.go
@@ -1,2 +1,37 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"github.com/ernestoaparicio/blog-example/blog/blogpb"
+	"google.golang.org/grpc"
+	"log"
+)
+
+func main() {
+	fmt.Println("Blog Client")
+
+	opts := grpc.WithInsecure()
+	cc, err := grpc.Dial("localhost:50051", opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer cc.Close()
+
+	c := blogpb.NewBlogServiceClient(cc)
+
+	createBlog(c)
+}
+func createBlog(client blogpb.BlogServiceClient) {
+	blog := &blogpb.Blog{
+		AuthorId: "Ernie",
+		Title:    "My first blog",
+		Content:  "Content of my first blog",
+	}
+	res, err := client.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{Blog: blog})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Blog has been created: %v", res)
+}
